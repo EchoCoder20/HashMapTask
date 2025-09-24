972630cf614df0aaadaa930e495492ef97c81750
EchoCoder20 <albusaidi1915@gmail.com>
2025-09-24 09:25:07 +0400

    Solving StudentRegistry Task

---
+++ StudentRegistry.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class StudentRegistry {
+    /**
+     * TO-DO: Invert the given map.
+     * The keys of the original map should become the values of the new map,
+     * and the values should become the keys.
+     \*
+     * @param studentIdToName A map from student ID (Integer) to student name (String).
+     * @return A new map from student name (String) to student ID (Integer).
+     */
+    public Map<String, Integer> invertMap(Map<Integer, String> studentIdToName) {
+        Map<String, Integer> nameToStudentId = new HashMap<>();
+        for (Map.Entry<Integer, String> data: studentIdToName.entrySet())
+        {
+            nameToStudentId.put(data.getValue(),data.getKey());
+        }
+        // Your code here. Iterate over the entrySet of the original map.
+        return nameToStudentId; // Placeholder
+    }
+
+
+
+
+}
